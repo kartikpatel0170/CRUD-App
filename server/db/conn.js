4901061af5a6b128ae949777ecb21c3f98450f2e
Kartik Patel <kartikpatel0170@gmail.com>
2022-11-09 17:39:12 -0600

    added conn.js

---
+++ server/db/conn.js
@@ -0,0 +1,7 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.DB,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(()=> console.log("connection start")).catch((error)=> console.log(error.message));
